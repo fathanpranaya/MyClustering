2c6283fcecd0a9bbd35776f6b8115a7cf9c87e52
fathanpranaya <fathan.pranaya@gmail.com>
2014-12-06 18:57:46 +0700

    update kmeans

--- src/MyKMeans.java
+++ src/MyKMeans.java
@@ -1,63 +1,15 @@
-/*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- *    SimpleKMeans.java
- *    Copyright (C) 2000-2012 University of Waikato, Hamilton, New Zealand
- *
- */
-
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.Vector;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
-import weka.classifiers.rules.DecisionTableHashKey;
-import weka.clusterers.Canopy;
 import weka.clusterers.FarthestFirst;
 import weka.clusterers.NumberOfClustersRequestable;
 import weka.clusterers.RandomizableClusterer;
 import weka.core.Attribute;
-import weka.core.Capabilities;
-import weka.core.Capabilities.Capability;
 import weka.core.DenseInstance;
 import weka.core.DistanceFunction;
 import weka.core.EuclideanDistance;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.ManhattanDistance;
-import weka.core.Option;
-import weka.core.RevisionUtils;
-import weka.core.SelectedTag;
-import weka.core.Tag;
-import weka.core.TechnicalInformation;
-import weka.core.TechnicalInformation.Field;
-import weka.core.TechnicalInformation.Type;
-import weka.core.TechnicalInformationHandler;
 import weka.core.Utils;
 import weka.core.WeightedInstancesHandler;
-import weka.filters.Filter;
-import weka.filters.unsupervised.attribute.ReplaceMissingValues;
 
 /**
  <!-- options-end -->
@@ -72,61 +24,54 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 	/** for serialization. */
 	static final long serialVersionUID = -3235809600124455376L;
 
-	protected ReplaceMissingValues m_ReplaceMissingFilter;
-	protected int m_NumClusters = 2; //parameter
-	protected Instances m_initialStartPoints;
-	protected Instances m_ClusterCentroids;
-	protected int[][][] m_ClusterNominalCounts;
-	protected double[] m_FullMeansOrMediansOrModes;
-	protected int[][] m_FullNominalCounts;
-	protected int[] m_ClusterSizes;
-	protected int m_MaxIterations = 500; //parameter
-	protected int m_Iterations = 0;
-	protected double[] m_squaredErrors;
-	protected DistanceFunction m_DistanceFunction = new EuclideanDistance();
+	protected int n_cluster = 4; //parameter
+	protected Instances initial_points;
+	protected Instances centroids;
+	protected int[][][] n_nominal;
+	protected double[] modes;
+	protected int[][] full_nominal;
+	protected int[] cluster_size;
+	protected int max_iterations = 500; //parameter
+	protected int iterations = 0;
+	protected double[] mse;
+	protected DistanceFunction distance_function = new EuclideanDistance();
+	
+	protected int method = 3;
 
-
-
-	public static final int FARTHEST_FIRST = 3;
-	protected int m_initializationMethod = FARTHEST_FIRST;
-
-	protected double m_minClusterDensity = 2;
+	protected double min_density = 2;
 
 	public MyKMeans() {
 		super();
-
 		m_SeedDefault = 10;
 		setSeed(m_SeedDefault);
 	}
 
 	@Override
 	public void buildClusterer(Instances data) throws Exception {
-		m_Iterations = 0;
-
-		m_ReplaceMissingFilter = new ReplaceMissingValues();
+		iterations = 0;
 		Instances instances = new Instances(data);
 
 		instances.setClassIndex(-1);
-		m_FullNominalCounts = new int[instances.numAttributes()][0];
+		full_nominal = new int[instances.numAttributes()][0];
 
-		m_FullMeansOrMediansOrModes = moveCentroid(0, instances, false, false);
+		modes = moveCentroid(0, instances, false, false);
 		for (int i = 0; i < instances.numAttributes(); i++) {
-			m_FullNominalCounts[i] = instances.attributeStats(i).nominalCounts;
+			full_nominal[i] = instances.attributeStats(i).nominalCounts;
 		}
 
-		m_ClusterCentroids = new Instances(instances, m_NumClusters);
+		centroids = new Instances(instances, n_cluster);
 		int[] clusterAssignments = new int[instances.numInstances()];
 
-		m_DistanceFunction.setInstances(instances);
+		distance_function.setInstances(instances);
 
 		Instances initInstances = null;
 		initInstances = instances;
 
 		//FarthestFirst init
 		farthestFirstInit(initInstances);
-		m_initialStartPoints = new Instances(m_ClusterCentroids);
+		initial_points = new Instances(centroids);
 
-		m_NumClusters = m_ClusterCentroids.numInstances();
+		n_cluster = centroids.numInstances();
 
 		// removing reference
 		initInstances = null;
@@ -134,16 +79,14 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 		int i;
 		boolean converged = false;
 		int emptyClusterCount;
-		Instances[] tempI = new Instances[m_NumClusters];
-		m_squaredErrors = new double[m_NumClusters];
-		m_ClusterNominalCounts = new int[m_NumClusters][instances.numAttributes()][0];
+		Instances[] tempI = new Instances[n_cluster];
+		mse = new double[n_cluster];
+		n_nominal = new int[n_cluster][instances.numAttributes()][0];
 		//startExecutorPool();
 
 		while (!converged) {
-
-
 			emptyClusterCount = 0;
-			m_Iterations++;
+			iterations++;
 			converged = true;
 
 			for (i = 0; i < instances.numInstances(); i++) {
@@ -160,15 +103,15 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 			}
 
 			// update centroids
-			m_ClusterCentroids = new Instances(instances, m_NumClusters);
-			for (i = 0; i < m_NumClusters; i++) {
+			centroids = new Instances(instances, n_cluster);
+			for (i = 0; i < n_cluster; i++) {
 				tempI[i] = new Instances(instances, 0);
 			}
 			for (i = 0; i < instances.numInstances(); i++) {
 				tempI[clusterAssignments[i]].add(instances.instance(i));
 			}
 
-			for (i = 0; i < m_NumClusters; i++) {
+			for (i = 0; i < n_cluster; i++) {
 				if (tempI[i].numInstances() == 0) {
 					// empty cluster
 					emptyClusterCount++;
@@ -177,52 +120,52 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 				}
 			}
 
-			if (m_Iterations == m_MaxIterations) {
+			if (iterations == max_iterations) {
 				converged = true;
 			}
 
 			if (emptyClusterCount > 0) {
-				m_NumClusters -= emptyClusterCount;
+				n_cluster -= emptyClusterCount;
 				if (converged) {
-					Instances[] t = new Instances[m_NumClusters];
+					Instances[] t = new Instances[n_cluster];
 					int index = 0;
 					for (int k = 0; k < tempI.length; k++) {
 						if (tempI[k].numInstances() > 0) {
 							t[index] = tempI[k];
 
 							for (i = 0; i < tempI[k].numAttributes(); i++) {
-								m_ClusterNominalCounts[index][i] = m_ClusterNominalCounts[k][i];
+								n_nominal[index][i] = n_nominal[k][i];
 							}
 							index++;
 						}
 					}
 					tempI = t;
 				} else {
-					tempI = new Instances[m_NumClusters];
+					tempI = new Instances[n_cluster];
 				}
 			}
 
 			if (!converged) {
-				m_ClusterNominalCounts = new int[m_NumClusters][instances
+				n_nominal = new int[n_cluster][instances
 				                                                .numAttributes()][0];
 			}
 		}
 
-		m_ClusterSizes = new int[m_NumClusters];
-		for (i = 0; i < m_NumClusters; i++) {
-			m_ClusterSizes[i] = tempI[i].numInstances();
+		cluster_size = new int[n_cluster];
+		for (i = 0; i < n_cluster; i++) {
+			cluster_size[i] = tempI[i].numInstances();
 		}
 
 		// save memory!
-		m_DistanceFunction.clean();
+		distance_function.clean();
 	}
 
 	protected void farthestFirstInit(Instances data) throws Exception {
 		FarthestFirst ff = new FarthestFirst();
-		ff.setNumClusters(m_NumClusters);
+		ff.setNumClusters(n_cluster);
 		ff.buildClusterer(data);
 
-		m_ClusterCentroids = ff.getClusterCentroids();
+		centroids = ff.getClusterCentroids();
 	}
 
 	protected double[] moveCentroid(int centroidIndex, Instances members,
@@ -234,17 +177,17 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 		int middle = 0;
 		boolean dataIsEven = false;
 
-		if (m_DistanceFunction instanceof ManhattanDistance) {
+		if (distance_function instanceof ManhattanDistance) {
 			middle = (members.numInstances() - 1) / 2;
 			dataIsEven = ((members.numInstances() % 2) == 0);
 			sortedMembers = new Instances(members);
 		}
 
 		for (int j = 0; j < members.numAttributes(); j++) {
-			if (m_DistanceFunction instanceof EuclideanDistance
+			if (distance_function instanceof EuclideanDistance
 					|| members.attribute(j).isNominal()) {
 				vals[j] = members.meanOrMode(j);
-			} else if (m_DistanceFunction instanceof ManhattanDistance) {
+			} else if (distance_function instanceof ManhattanDistance) {
 				// singleton special case
 				if (members.numInstances() == 1) {
 					vals[j] = members.instance(0).value(j);
@@ -257,11 +200,11 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 			}
 
 			if (updateClusterInfo) {
-				m_ClusterNominalCounts[centroidIndex][j] = members.attributeStats(j).nominalCounts;
+				n_nominal[centroidIndex][j] = members.attributeStats(j).nominalCounts;
 			}
 		}
 		if (addToCentroidInstances) {
-			m_ClusterCentroids.add(new DenseInstance(1.0, vals));
+			centroids.add(new DenseInstance(1.0, vals));
 		}
 		return vals;
 	}
@@ -270,17 +213,17 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 			boolean useFastDistCalc, long[] instanceCanopies) {
 		double minDist = Integer.MAX_VALUE;
 		int bestCluster = 0;
-		for (int i = 0; i < m_NumClusters; i++) {
+		for (int i = 0; i < n_cluster; i++) {
 			double dist;
 			if (useFastDistCalc) {
 
 
-				dist = m_DistanceFunction.distance(instance,
-						m_ClusterCentroids.instance(i), minDist);
+				dist = distance_function.distance(instance,
+						centroids.instance(i), minDist);
 
 			} else {
-				dist = m_DistanceFunction.distance(instance,
-						m_ClusterCentroids.instance(i));
+				dist = distance_function.distance(instance,
+						centroids.instance(i));
 			}
 			if (dist < minDist) {
 				minDist = dist;
@@ -288,11 +231,11 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 			}
 		}
 		if (updateErrors) {
-			if (m_DistanceFunction instanceof EuclideanDistance) {
+			if (distance_function instanceof EuclideanDistance) {
 				// Euclidean distance to Squared Euclidean distance
 				minDist *= minDist;
 			}
-			m_squaredErrors[bestCluster] += minDist;
+			mse[bestCluster] += minDist;
 		}
 		return bestCluster;
 	}
@@ -306,48 +249,48 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 
 	@Override
 	public int numberOfClusters() throws Exception {
-		return m_NumClusters;
+		return n_cluster;
 	}
 
 	public void setNumClusters(int n) throws Exception {
 		if (n <= 0) {
 			throw new Exception("Number of clusters must be > 0");
 		}
-		m_NumClusters = n;
+		n_cluster = n;
 	}
 
 	public void setMaxIterations(int n) throws Exception {
 		if (n <= 0) {
 			throw new Exception("Maximum number of iterations must be > 0");
 		}
-		m_MaxIterations = n;
+		max_iterations = n;
 	}
 
 	public int getMaxIterations() {
-		return m_MaxIterations;
+		return max_iterations;
 	}
 
 	@Override
 	public String toString() {
-		if (m_ClusterCentroids == null) {
+		if (centroids == null) {
 			return "No clusterer built yet!";
 		}
 
 		int maxWidth = 0;
 		int maxAttWidth = 0;
-		for (int i = 0; i < m_NumClusters; i++) {
-			for (int j = 0; j < m_ClusterCentroids.numAttributes(); j++) {
-				if (m_ClusterCentroids.attribute(j).name().length() > maxAttWidth) {
-					maxAttWidth = m_ClusterCentroids.attribute(j).name().length();
+		for (int i = 0; i < n_cluster; i++) {
+			for (int j = 0; j < centroids.numAttributes(); j++) {
+				if (centroids.attribute(j).name().length() > maxAttWidth) {
+					maxAttWidth = centroids.attribute(j).name().length();
 				}
 			}
 		}
 
-		for (int i = 0; i < m_ClusterCentroids.numAttributes(); i++) {
-			if (m_ClusterCentroids.attribute(i).isNominal()) {
-				Attribute a = m_ClusterCentroids.attribute(i);
-				for (int j = 0; j < m_ClusterCentroids.numInstances(); j++) {
-					String val = a.value((int) m_ClusterCentroids.instance(j).value(i));
+		for (int i = 0; i < centroids.numAttributes(); i++) {
+			if (centroids.attribute(i).isNominal()) {
+				Attribute a = centroids.attribute(i);
+				for (int j = 0; j < centroids.numInstances(); j++) {
+					String val = a.value((int) centroids.instance(j).value(i));
 					if (val.length() > maxWidth) {
 						maxWidth = val.length();
 					}
@@ -362,7 +305,7 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 		}
 
 		// check for size of cluster sizes
-		for (int m_ClusterSize : m_ClusterSizes) {
+		for (int m_ClusterSize : cluster_size) {
 			String size = "(" + m_ClusterSize + ")";
 			if (size.length() > maxWidth) {
 				maxWidth = size.length();
@@ -384,48 +327,48 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 
 		StringBuffer temp = new StringBuffer();
 		temp.append("\nMy KMeans\n======\n");
-		temp.append("\nNumber of iterations: " + m_Iterations);
+		temp.append("\nNumber of iterations: " + iterations);
 
 		temp.append("\n\nInitial staring points (");
 
 		temp.append("farthest first");
 		temp.append("):\n");
 		temp.append("\n\nFinal cluster centroids:\n");
-		temp.append(pad("Cluster#", " ", (maxAttWidth + (maxWidth * 2 + 2))
+		temp.append(part("Cluster#", " ", (maxAttWidth + (maxWidth * 2 + 2))
 				- "Cluster#".length(), true));
 
 		temp.append("\n");
 		temp
-		.append(pad("Attribute", " ", maxAttWidth - "Attribute".length(), false));
+		.append(part("Attribute", " ", maxAttWidth - "Attribute".length(), false));
 
 		temp
-		.append(pad("Full Data", " ", maxWidth + 1 - "Full Data".length(), true));
+		.append(part("Full Data", " ", maxWidth + 1 - "Full Data".length(), true));
 
 		// cluster numbers
-		for (int i = 0; i < m_NumClusters; i++) {
+		for (int i = 0; i < n_cluster; i++) {
 			String clustNum = "" + i;
-			temp.append(pad(clustNum, " ", maxWidth + 1 - clustNum.length(), true));
+			temp.append(part(clustNum, " ", maxWidth + 1 - clustNum.length(), true));
 		}
 		temp.append("\n");
 
 		// cluster sizes
-		String cSize = "(" + Utils.sum(m_ClusterSizes) + ")";
-		temp.append(pad(cSize, " ", maxAttWidth + maxWidth + 1 - cSize.length(),
+		String cSize = "(" + Utils.sum(cluster_size) + ")";
+		temp.append(part(cSize, " ", maxAttWidth + maxWidth + 1 - cSize.length(),
 				true));
-		for (int i = 0; i < m_NumClusters; i++) {
-			cSize = "(" + m_ClusterSizes[i] + ")";
-			temp.append(pad(cSize, " ", maxWidth + 1 - cSize.length(), true));
+		for (int i = 0; i < n_cluster; i++) {
+			cSize = "(" + cluster_size[i] + ")";
+			temp.append(part(cSize, " ", maxWidth + 1 - cSize.length(), true));
 		}
 		temp.append("\n");
 
-		temp.append(pad("", "=",
+		temp.append(part("", "=",
 				maxAttWidth
-				+ (maxWidth * (m_ClusterCentroids.numInstances() + 1)
-						+ m_ClusterCentroids.numInstances() + 1), true));
+				+ (maxWidth * (centroids.numInstances() + 1)
+						+ centroids.numInstances() + 1), true));
 		temp.append("\n");
 
-		for (int i = 0; i < m_ClusterCentroids.numAttributes(); i++) {
-			String attName = m_ClusterCentroids.attribute(i).name();
+		for (int i = 0; i < centroids.numAttributes(); i++) {
+			String attName = centroids.attribute(i).name();
 			temp.append(attName);
 			for (int j = 0; j < maxAttWidth - attName.length(); j++) {
 				temp.append(" ");
@@ -434,16 +377,16 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 			String strVal;
 			String valMeanMode;
 			// full data
-			valMeanMode = pad(
-					(strVal = m_ClusterCentroids.attribute(i).value(
-							(int) m_FullMeansOrMediansOrModes[i])), " ", maxWidth + 1
+			valMeanMode = part(
+					(strVal = centroids.attribute(i).value(
+							(int) modes[i])), " ", maxWidth + 1
 							- strVal.length(), true);
 			temp.append(valMeanMode);
 
-			for (int j = 0; j < m_NumClusters; j++) {
-				valMeanMode = pad(
-						(strVal = m_ClusterCentroids.attribute(i).value(
-								(int) m_ClusterCentroids.instance(j).value(i))), " ", maxWidth
+			for (int j = 0; j < n_cluster; j++) {
+				valMeanMode = part(
+						(strVal = centroids.attribute(i).value(
+								(int) centroids.instance(j).value(i))), " ", maxWidth
 								+ 1 - strVal.length(), true);
 				temp.append(valMeanMode);
 			}
@@ -454,7 +397,7 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 		return temp.toString();
 	}
 
-	private String pad(String source, String padChar, int length, boolean leftPad) {
+	private String part(String source, String padChar, int length, boolean leftPad) {
 		StringBuffer temp = new StringBuffer();
 
 		if (leftPad) {
@@ -472,24 +415,19 @@ NumberOfClustersRequestable, WeightedInstancesHandler {
 	}
 
 	public Instances getClusterCentroids() {
-		return m_ClusterCentroids;
+		return centroids;
 	}
 
 	public int[][][] getClusterNominalCounts() {
-		return m_ClusterNominalCounts;
+		return n_nominal;
 	}
 
 	public double getSquaredError() {
-		return Utils.sum(m_squaredErrors);
+		return Utils.sum(mse);
 	}
 
 	public int[] getClusterSizes() {
-		return m_ClusterSizes;
+		return cluster_size;
 	}
-
-	public static void main(String[] args) {
-		runClusterer(new MyKMeans(), args);
-	}
-
 }
 
